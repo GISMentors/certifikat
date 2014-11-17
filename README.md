f46256bd199a295a520cdb1d7d677fcd2883522c
Jachym Cepicky <jachym.cepicky@gmail.com>
2014-11-17 21:28:59 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+Certifikáty GISMentors
+======================
+Šablony uloženy v adresáři `templates`
+
+pro vygenerování použít např.:
+
+    python3 generate-certificate.py --template templates/grass-zacatecnik.tex --name "Ing. Pepa Popovič" --date "10. -- 11. listopadu 2014" --date-signed "11. listopadu 2014" --output-file pokus1.tex
+    cd certificates/
+    pdflatex pokus1.tex
---
+++ generate-certificate.py
@@ -0,0 +1,33 @@
+import argparse
+import re
+import os
+
+def generate(templatefile, name, date, date_signed, output_file):
+    """Generate output .tex file based on given data and template
+    """
+
+    template_content = open(templatefile).read()
+    template_content = re.sub(r'\[name\]', name, template_content)
+    template_content = re.sub(r'\[date\]', date, template_content)
+    template_content = re.sub(r'\[date-signed\]', date_signed, template_content)
+
+    output_file = os.path.join('certificates', output_file)
+    outputfile = open(output_file, 'w')
+    outputfile.write(template_content)
+    outputfile.close()
+
+    print("Output written to %s" % (output_file))
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate certificate')
+    parser.add_argument('--template', required=True, help='Soubor .tex se šablonou')
+    parser.add_argument('--name', required=True, help='Jméno účastníka')
+    parser.add_argument('--date', required=True, help='Datum konání (použijte zápis pro LaTeX')
+    parser.add_argument('--date-signed', required=True, help='Datum podpisu (použijte zápis pro LaTeX')
+    parser.add_argument('--output-file', required=True, help='Jméno výstupního .tex souboru (bude uloženo do adresáře "certificates")')
+
+    args = parser.parse_args()
+    generate(args.template, args.name, args.date, args.date_signed, args.output_file)
+
+if __name__ == '__main__':
+    main()
