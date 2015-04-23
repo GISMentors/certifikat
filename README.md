09f4838fe2165a95e9e40fa3493d9cae926927f4
Jachym Cepicky <jachym.cepicky@gmail.com>
2015-04-23 11:37:46 +0200

    adding 'place' parameter

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Certifikáty GISMentors
 
 pro vygenerování použít např.:
 
-    python generate-certificate.py --template templates/grass-zacatecnik.tex \
+    python3 generate-certificate.py --template templates/grass-zacatecnik.tex \
     --name "Ing. Pepa Popovič" --date "10. -- 11. listopadu 2014" \
+    --place "Olomouci" \
     --date-signed "11. listopadu 2014" --output-file pokus1.tex
     (cd certificates/ && pdflatex pokus1.tex && xpdf pokus1.pdf)
-    
--- generate-certificate.py
+++ generate-certificate.py
@@ -4,11 +4,12 @@ import argparse
 import re
 import os
 
-def generate(templatefile, name, date, date_signed, output_file):
+def generate(templatefile, name, date, date_signed, output_file, place):
     """Generate output .tex file based on given data and template
     """
 
     template_content = open(templatefile).read()
+    template_content = re.sub(r'\[place\]', place, template_content)
     template_content = re.sub(r'\[name\]', name, template_content)
     template_content = re.sub(r'\[date\]', date, template_content)
     template_content = re.sub(r'\[date-signed\]', date_signed, template_content)
@@ -27,11 +28,12 @@ def main():
     parser.add_argument('--template', required=True, help='Soubor .tex se šablonou')
     parser.add_argument('--name', required=True, help='Jméno účastníka')
     parser.add_argument('--date', required=True, help='Datum konání (použijte zápis pro LaTeX')
+    parser.add_argument('--place', required=True, help='Místo konání')
     parser.add_argument('--date-signed', required=True, help='Datum podpisu (použijte zápis pro LaTeX')
     parser.add_argument('--output-file', required=True, help='Jméno výstupního .tex souboru (bude uloženo do adresáře "certificates")')
 
     args = parser.parse_args()
-    generate(args.template, args.name, args.date, args.date_signed, args.output_file)
+    generate(args.template, args.name, args.date, args.date_signed, args.output_file, args.place)
 
 if __name__ == '__main__':
     main()
